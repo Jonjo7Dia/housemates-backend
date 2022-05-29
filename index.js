49b323f28961f0f41d30e585932b86ccd6f98169
Jonjo7Dia <jonosphgames@gmail.com>
2022-05-29 17:23:30 -0300

    try to signup

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require('./db');
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
+
 
 //database connection 
 connection();
+
 //middlewares
 app.use(express.json());
 app.use(cors());
+
+//routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 8080;
 
